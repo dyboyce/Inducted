0c8850b6abfd52ea34f524e90bcb7d5151643ea5
dylan <dylanboyce.206@gmail.com>
2017-02-13 15:19:10 -0800

    Added lifecycle generics

--- app/src/main/java/net/dboyce/inducted/MainActivity.java
+++ app/src/main/java/net/dboyce/inducted/MainActivity.java
@@ -15,11 +15,60 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    protected  void onStart() {
+        System.out.print("onStart called.");
+        //placeholder for onstart mechanics
+
+    }
+
+    @Override
+    protected  void onRestart() {
+        System.out.print("onRestart called.");
+        //placeholder for onRestart mechanics
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        System.out.print("onResume called.");
+        //placeholder for onResume mechanics
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();  // Always call the superclass method first
+        System.out.print("onPause called.");
+        //placeholder for onPause mechanics
+
+    }
+
+    @Override
+    protected void onStop() {
+        System.out.print("onStop called.");
+        //placeholder for onStop mechanics
+        //save application data here instead of onPause
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        System.out.print("onDestroy called.");
+        //placeholder for onDestroy mechanics
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
