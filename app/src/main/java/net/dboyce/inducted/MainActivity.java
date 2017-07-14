2077f6adabb930c0044ba4c569e5209c5898fbda
dyboyce <dylanboyce.206@gmail.com>
2017-07-14 07:43:01 -0700

    Menu Fragment Stabilizing

--- app/src/main/java/net/dboyce/inducted/MainActivity.java
+++ app/src/main/java/net/dboyce/inducted/MainActivity.java
@@ -1,30 +1,58 @@
 package net.dboyce.inducted;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.support.v4.app.Fragment;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements settingsFragment.OnFragmentInteractionListener {
+
+    //we want to get the last induction from memory and start it
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            // These flags cause the device screen to turn on (and bypass screen guard if possible) when launching.
+            // This makes it easy for developers to test the app launch without needing to turn on the device
+            // each time and without needing to enable the "Stay awake" option.
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("inducted","oncreate called");
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        settingsFragment sf = new settingsFragment();
+        ft.add(R.id.mainact, sf);
+        ft.commit();
+
     }
 
     @Override
     protected  void onStart() {
         super.onStart();
-        System.out.print("onStart called.");
+        Log.d("inducted","onStart called");
         //placeholder for onstart mechanics
 
     }
 
     @Override
     protected  void onRestart() {
-        System.out.print("onRestart called.");
+        super.onRestart();
+        Log.d("inducted","onRestart called");
         //placeholder for onRestart mechanics
 
     }
@@ -33,7 +61,7 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();  // Always call the superclass method first
 
-        System.out.print("onResume called.");
+        Log.d("inducted","onResume called");
         //placeholder for onResume mechanics
 
     }
@@ -41,14 +69,15 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();  // Always call the superclass method first
-        System.out.print("onPause called.");
+        Log.d("inducted","onPause called");
         //placeholder for onPause mechanics
 
     }
 
     @Override
     protected void onStop() {
-        System.out.print("onStop called.");
+        super.onStop();
+        Log.d("inducted","onStop called.");
         //placeholder for onStop mechanics
         //save application data here instead of onPause
 
@@ -57,7 +86,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-        System.out.print("onDestroy called.");
+        super.onDestroy();
+        Log.d("inducted","onDestroy called");
         //placeholder for onDestroy mechanics
 
     }
@@ -84,5 +114,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //Fragment mabob
     }
 }
